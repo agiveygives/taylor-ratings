c424f1f6c6d6248047093d5d1b0792a3aa90f0a2
agiveygives <justincase25xxv@gmail.com>
2024-04-30 23:31:07 -0500

    group albums with the same average and show top song

--- src/routes/albums/results/+page.svelte
+++ src/routes/albums/results/+page.svelte
@@ -3,6 +3,36 @@
   import { allAlbumsData } from '$lib/data/albums';
 
 	let albumRatings = sortAlbumsByRatings($ratings, "average");
+
+  let currentPlace = 0;
+  let groupRatings = albumRatings.reduce((acc, albumRating) => {
+    albumRating.topSong = topSong(albumRating.songRatings);
+
+    if (currentPlace === 0) {
+      currentPlace += 1;
+      acc[currentPlace] = [albumRating];
+    } else if (acc[currentPlace][0].average === albumRating.average) {
+      acc[currentPlace].push(albumRating);
+    } else {
+      currentPlace += 1;
+      acc[currentPlace] = [albumRating];
+    }
+
+    return acc;
+  }, {});
+
+  function topSong(songRatings) {
+    return Object.entries(songRatings).reduce((acc, [title, rating]) => {
+      if (rating === undefined || rating === null) return acc;
+
+      if (rating > acc.rating) {
+        acc.title = title;
+        acc.rating = rating;
+      }
+
+      return acc;
+    }, { title: '--', rating: 0 }).title;
+  }
 </script>
 
 <div>
@@ -12,16 +42,41 @@
         <th>Place</th>
         <th>Album</th>
         <th>Average Rating</th>
+        <th>Top Song</th>
       </tr>
     </thead>
     <tbody>
-      {#each albumRatings as albumRating, i}
+      {#each Object.entries(groupRatings) as [place, albums]}
         <tr>
-          <td>{i + 1}</td>
-          <td>{albumRating.title}</td>
-          <td>{albumRating.average}</td>
+          <td rowspan={albums.length}>{place}</td>
+          <td>{albums[0].title}</td>
+          <td>{albums[0].average ? albums[0].average.toFixed(4) : 'unrated'}</td>
+          <td>{albums[0].topSong}</td>
         </tr>
+        {#each albums.slice(1) as album}
+          <tr>
+            <td>{album.title}</td>
+            <td>{album.average ? album.average.toFixed(4) : 'unrated'}</td>
+            <td>{album.topSong}</td>
+          </tr>
+        {/each}
       {/each}
     </tbody>
   </table>
 </div>
+
+<style>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+
+  th, td {
+    border: 1px solid black;
+    padding: 8px;
+  }
+
+  th {
+    background-color: #f2f2f2;
+  }
+</style>
